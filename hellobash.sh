40c1c8c8764f3fa2f55443d59b0d3a1b3c283d08
root <root@GreenCloud.1657497472>
2022-07-11 05:55:37 -0400

    2st

--- hellobash.sh
+++ hellobash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo 'hellobash'
+apt-get update
+chmod 777 /root
 apt-get -y install curl wget git golang clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev 
 curl -fsSL https://code-server.dev/install.sh | sh
 #systemctl restart code-server@$USER
@@ -8,4 +10,3 @@ sed -i '/bind-addr:/c\bind-addr: 0.0.0.0:888' /root/.config/code-server/config.y
 sed -i '/password:/c\password: rui333435' /root/.config/code-server/config.yaml
 git clone https://github.com/flutter/flutter.git -b stable
 sed -i '$a\export PATH="$PATH:/root/flutter/bin"' /etc/profile
-flutter precache
