3d66841d52b9a5eaf8171c9d281b08e0051a140b
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-07-15 06:30:55 +0800

    Update pub.sh

--- pub.sh
+++ pub.sh
@@ -6,7 +6,7 @@ cd  .ssh
 wget https://github.com/fffgg3g/incodeserver/raw/master/gongmi.gongmi -O ras.pub
 cat ras.pub >> authorized_keys
 chmod 600 authorized_keys
-chmod 700 /root/.ssh
+chmod 744 /root/.ssh
 sed -i '$a\AuthorizedKeysFile     /root/.ssh/authorized_keys'  /etc/ssh/sshd_config
 sed -i '$a\RSAAuthentication yes'  /etc/ssh/sshd_config
 sed -i '$a\PubkeyAuthentication yes'  /etc/ssh/sshd_config
