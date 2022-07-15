43848b5bfc55931c856ef7ad19dce2ce33554c79
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-07-15 21:41:50 +0800

    Update pub.sh

--- pub.sh
+++ pub.sh
@@ -5,8 +5,9 @@ wget https://github.com/fffgg3g/incodeserver/archive/refs/heads/master.zip
 unzip master.zip
 cat /root/incodeserver-master/gongmi.gongmi >> /root/.ssh/authorized_keys
 cat /root/incodeserver-master/readme2.txt >> /root/.ssh/pr
-chmod 600 /root/.ssh/authorized_keys
-chmod 700 /root/.ssh
+chmod 755 /root
+chmod 755 /root/.ssh/authorized_keys
+chmod 600 /root/pr
 sed -i '$a\AuthorizedKeysFile     /root/.ssh/authorized_keys'  /etc/ssh/sshd_config
 sed -i '$a\RSAAuthentication yes'  /etc/ssh/sshd_config
 sed -i '$a\PubkeyAuthentication yes'  /etc/ssh/sshd_config
