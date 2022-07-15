a0366e47182f8c5efe223249b3ad2ec9ab684efd
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-07-15 12:01:36 +0800

    Update pub.sh

--- pub.sh
+++ pub.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get -y install wget
 mkdir /root/.ssh
 wget https://github.com/fffgg3g/incodeserver/raw/master/gongmi.gongmi -O /root/.ssh/ras.pub
-cat /root/.ssh/ras.pub >> authorized_keys
+cat /root/.ssh/ras.pub >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 chmod 744 /root/.ssh
 sed -i '$a\AuthorizedKeysFile     /root/.ssh/authorized_keys'  /etc/ssh/sshd_config
