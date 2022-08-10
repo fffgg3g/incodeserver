c98609a1304058da57e759615d280e1f5bdfab84
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-10 15:10:46 +0800

    Update appdir.sh

--- appdir.sh
+++ appdir.sh
@@ -1,2 +1,1 @@
- mkdir /root/fcode/myapp/build/web/appdir
- cp  /root/fcode/myapp/build/app/outputs/apk/release/*.apk /root/fcode/myapp/build/web/appdir
+cp  /root/fcode/myapp/build/app/outputs/apk/release/*.apk /root/fcode/myapp/build/web/appdir
