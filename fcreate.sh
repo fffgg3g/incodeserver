dbb551d9ae6e91c5a06b08fc291412f291b740e0
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-12 07:29:42 +0800

    Update fcreate.sh

--- fcreate.sh
+++ fcreate.sh
@@ -7,6 +7,8 @@ sleep 30
 cd $1
 flutter build web
 sleep 30
+cp /root/appdir.sh /root/fcode/$1
+chmod 777 /root/fcode/$1/appdir.sh
 sed -i "/root/c\root /root/fcode\/$1/build/web;" /etc/nginx/sites-enabled/default
 reboot
 
