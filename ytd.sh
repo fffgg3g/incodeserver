3b178294c8b33c676185bdf1f4a1cb36cfd30cf8
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-15 05:21:42 +0800

    Update ytd.sh

--- ytd.sh
+++ ytd.sh
@@ -2,8 +2,8 @@
 yt-dlp -F $1 >yt.txt
 sed -n '/audio/w au.txt' yt.txt
 sed -n '/video/w vi.txt' yt.txt
-sed -n '/audio/{/m4a/p}' au.txt  >au1.txt 
-sed -n '/video/{/mp4/p}' vi.txt  >vi1.txt
+sed -n '/audio/{/webm/p}' au.txt  >au1.txt 
+sed -n '/video/{/webm/p}' vi.txt  >vi1.txt
 aunumber=$(sed -n '$p' au1.txt | awk '{print $1}' )
 vinumber=$(sed -n '$p' vi1.txt | awk '{print $1}' )
 yt-dlp -f $vinumber+$aunumber $1
