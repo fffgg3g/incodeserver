cb9bdf94498ba0a45a80b91c5131e1645299256f
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-14 19:40:49 +0800

    Update ytdown.sh

--- ytdown.sh
+++ ytdown.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-apt-get -y install wget python3 python3-pip  ffmpeg
-wget https://github.com/fffgg3g/incodeserver/raw/master/nginx.conf
-systemctl stop nginx
-\cp nginx.conf /etc/nginx
-mkdir getdowns
-chmod 755 -R /root/getdowns
-mkdir getdowns/vv
-systemctl restart nginx
-pip install yt-dlp
-pip install -U yt-dlp
 yt-dlp -F $1 >yt.txt
 sed -n '/audio/w au.txt' yt.txt
 sed -n '/video/w vi.txt' yt.txt
