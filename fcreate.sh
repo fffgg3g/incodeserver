04c07c776c429d023cf4a7bb80a96979c33a6e75
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-15 06:46:18 +0800

    Update fcreate.sh

--- fcreate.sh
+++ fcreate.sh
@@ -8,11 +8,11 @@ cd $1
 flutter build web
 sleep 30
 mkdir /root/fcode/$1/build/web/vv
-echo "\cp /root/fcode/$1/build/app/outputs/apk/release/*.apk /root/fcode/$1/build/web/vv" >/root/fcode/$1/appto.sh
-chmod 777 /root/fcode/$1/appto.sh
+echo "\cp /root/fcode/$1/build/app/outputs/apk/release/*.apk /root/fcode/$1/build/web/vv" >/usr/local/bin/appto
+chmod 777 /usr/local/bin/appto
 ######cyoutubedown
-cp /root/fcode/ytd.sh /root/fcode/$1/build/web/vv
-chmod 777 /root/fcode/$1/build/web/vv/ytd.sh
+###cp /root/fcode/ytd /root/fcode/$1/build/web/vv
+####chmod 777 /root/fcode/$1/build/web/vv/ytd
 #######
 sed -i "/root/c\root /root/fcode\/$1/build/web;" /etc/nginx/sites-enabled/default
 reboot
