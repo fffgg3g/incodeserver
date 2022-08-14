96e4b61ab143e8e42f67b170f33428a00eb04aea
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-15 06:33:53 +0800

    Update v2ytdinstall.sh

--- v2ytdinstall.sh
+++ v2ytdinstall.sh
@@ -7,8 +7,8 @@ mkdir getdowns
 chmod 755 -R /root
 chmod 755 -R /root/getdowns
 mkdir getdowns/vv
-wget -P /usr/local/bin/ https://raw.githubusercontent.com/fffgg3g/incodeserver/master/ytd
-chmod 777 /usr/local/bin/ytd
+wget -P /usr/local/bin/ https://raw.githubusercontent.com/fffgg3g/incodeserver/master/ytdd
+chmod 777 /usr/local/bin/ytdd
 systemctl restart nginx
 pip install yt-dlp
 pip install -U yt-dlp
